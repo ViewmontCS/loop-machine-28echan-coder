56f83fed52094d30fab8f82e3628bb6a19058840
28echan-coder <28echan@go.dsdmail.net>
2025-11-25 15:51:05 +0000

    HOOOORAY :) he he he haw

--- main.py
+++ main.py
@@ -10,25 +10,45 @@ score = 1500
 cost = 100
 payout = 0
 print("Welcome to the slot machine!")
-while score >= cost:
-  sleep(0.8)
-  score = score - cost
-  slot = [[1,2,3],[4,5,6,],[7,8,9]]
-  for row in slot:
-      for num in row:
-          num = randint(1,9)
+start = input("Do you want to play? (y or n) ")
+if start == "y":
+  while score >= cost:
+    sleep(0.4)
+    score = score - cost
+    slot = [[1,2,3],[4,5,6,],[7,8,9]]
+    for i in range(len(slot)):
+        for j in range(len(slot[i])):
+            slot[i][j] = randint(1,6)
+    for row in slot:
+        for num in row:
           print(f"|{num}|",end="")
-      print()
-
-slot = [[num, num, num][num, num, num][num, num, num]]
-if slot[0][0] == slot[0][1] == slot[0][2]:
-  print("hooray")
-
-  play = input("Do you want to play again? (y or n): ")
-  if play.lower() == "y":
-     continue
-  if play.lower() == "n":
-    break
+        print()
+    if slot[0][0] == slot[0][1] and slot[0][0] == slot[0][2]:
+       print("JACKPOT!!!")
+       score = score + 1000
+    if slot[1][0] == slot[1][1] and slot[1][0] == slot[1][2]:
+       print("JACKPOT!!!")
+       score = score + 1000
+    if slot[2][0] == slot[2][1] and slot[2][0] == slot[2][2]:
+       print("JACKPOT!!!")
+       print("JACKPOT!!!")
+       print("JACKPOT!!!")
+       score = score + 1000
+    if slot[0][0] == slot[1][1] and slot[0][0] == slot[2][2]:
+       print("JACKPOT!!!")
+       score = score + 1000
+    if slot[2][0] == slot[1][1] and slot[2][0] == slot[0][2]:
+       print("JACKPOT!!!")
+       print("JACKPOT!!!")
+       print("JACKPOT!!!")
+       score = score + 1000
+    play = input("Do you want to play again? (y or n): ")
+    if play.lower() == "y":
+      continue
+    if play.lower() == "n":
+      break
   print("Thanks for playing")
+if start == "n":
+   print("Then get out of here")
 if score < cost:
    print("Sorry, you can't play any more since you ran out of score.")
